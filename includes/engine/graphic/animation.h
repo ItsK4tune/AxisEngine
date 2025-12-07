a05968811b223f0a2f979e3110c489e65a56ccdd
ItsK4tune <duongcuteyb@gmail.com>
2025-12-07 20:32:28 +0700

    optimize: animation

--- includes/engine/graphic/animation.h
+++ includes/engine/graphic/animation.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <map>
+#include <unordered_map>
 #include <glm/glm.hpp>
 #include <assimp/scene.h>
 
@@ -29,7 +30,7 @@ public:
 	inline float GetTicksPerSecond() { return m_TicksPerSecond; }
 	inline float GetDuration() { return m_Duration; }
 	inline const AssimpNodeData &GetRootNode() { return m_RootNode; }
-	inline const std::map<std::string, BoneInfo> &GetBoneIDMap()
+	inline const std::unordered_map<std::string, BoneInfo> &GetBoneIDMap()
 	{
 		return m_BoneInfoMap;
 	}
@@ -38,8 +39,10 @@ private:
 	float m_Duration;
 	int m_TicksPerSecond;
 	std::vector<Bone> m_Bones;
+	std::unordered_map<std::string, Bone *> m_BoneMap;
+
 	AssimpNodeData m_RootNode;
-	std::map<std::string, BoneInfo> m_BoneInfoMap;
+	std::unordered_map<std::string, BoneInfo> m_BoneInfoMap;
 
 	void ReadMissingBones(const aiAnimation *animation, Model &model);
 	void ReadHierarchyData(AssimpNodeData &dest, const aiNode *src);
