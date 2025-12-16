b7140c5fba53ef88bc0f2085ba47c2586b33ab74
ItsK4tune <duongcuteyb@gmail.com>
2025-12-17 02:34:00 +0700

    update: scriptable

--- includes/engine/core/scriptable.h
+++ includes/engine/core/scriptable.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <engine/core/scene.h>
+#include <engine/ecs/component.h>
 
 class Application;
 
@@ -13,22 +14,33 @@ public:
     virtual void OnUpdate(float dt) {}
     virtual void OnDestroy() {}
 
-    template<typename T>
-    T& GetComponent()
+    template <typename T>
+    T &GetComponent()
     {
         return m_Scene->registry.get<T>(m_Entity);
     }
-    
-    template<typename T>
+
+    template <typename T>
     bool HasComponent()
     {
         return m_Scene->registry.all_of<T>(m_Entity);
     }
 
+    template <typename T>
+    T *GetScript(entt::entity targetEntity)
+    {
+        if (m_Scene->registry.all_of<ScriptComponent>(targetEntity))
+        {
+            auto &nsc = m_Scene->registry.get<ScriptComponent>(targetEntity);
+            return dynamic_cast<T *>(nsc.instance);
+        }
+        return nullptr;
+    }
+
 protected:
     entt::entity m_Entity;
-    Scene* m_Scene = nullptr;
-    Application* m_App = nullptr;
-    
+    Scene *m_Scene = nullptr;
+    Application *m_App = nullptr;
+
     friend class ScriptableSystem;
 };
