9d534f9bab502cbed816224f2674dfcfa6665a21
ItsK4tune <duongcuteyb@gmail.com>
2026-07-22 18:31:39 +0700

    fix: error shader

--- include/engine/asset/shaders/error_deferred.fs
+++ include/engine/asset/shaders/error_deferred.fs
@@ -16,7 +16,6 @@ in vec3 FragPos;
 in vec3 Normal;
 
 uniform uint u_EntityID;
-flat in uint EntityID;
 
 void main() {
 
@@ -31,7 +30,7 @@ void main() {
 
     gEmissive = mix(vec3(1.0, 0.0, 1.0), vec3(0.0, 0.0, 0.0), m);
 
-    gEntityID = EntityID;
+    gEntityID = u_EntityID;
 
 }
 
